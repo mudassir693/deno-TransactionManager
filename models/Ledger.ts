f8b059f1973417de3360acf17688f9caff062034
mudassir619 <mudassirsiddiqui27@gmail.com>
2022-07-28 01:34:51 +0500

    interfaces added.

---
+++ models/Ledger.ts
@@ -0,0 +1,8 @@
+interface Ledger {
+    TransactionType:string // Order or Payment,
+    Balance:number
+    PreviousBalance:number
+    Credit:number|string
+    debit:number|string
+    Date:string
+}
---
+++ models/Payment.ts
@@ -0,0 +1,9 @@
+interface Payment {
+    LedgerId:string,
+    Amount:number,
+    Date: string,
+    Type:string,
+    SerialNumber:string
+}
+
+export default Payment
